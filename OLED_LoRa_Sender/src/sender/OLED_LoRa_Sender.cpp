772cc7122f24b57adfbfdf7f328850b8a27c3e32
Huy Le <dacrazyazn@gmail.com>
2022-01-21 16:45:44 -0700

    sender is building and is able to move platformio downloaded libraries into local lib folder and get things to work

---
+++ OLED_LoRa_Sender/src/sender/OLED_LoRa_Sender.cpp
@@ -0,0 +1,117 @@
+// https://randomnerdtutorials.com/ttgo-lora32-sx1276-arduino-ide/
+
+#include <Arduino.h>
+#include <LoRa.h>
+#include <SPI.h>
+
+// Libraries for OLED Display
+#include <Adafruit_I2CDevice.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+
+#define SCK 5   // GPIO5  -- SX1278's SCK
+#define MISO 19 // GPIO19 -- SX1278's MISnO
+#define MOSI 27 // GPIO27 -- SX1278's MOSI
+#define SS 18   // GPIO18 -- SX1278's CS
+#define RST 14  // GPIO14 -- SX1278's RESET
+#define DI0 26  // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND 868E6
+
+unsigned int counter = 0;
+
+// SSD1306 display(0x3c, 21, 22);
+// String rssi = "RSSI --";
+// String packSize = "--";
+// String packet;
+
+// OLED pins
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+void setup() {
+  pinMode(OLED_RST, OUTPUT);
+  pinMode(2, OUTPUT);
+
+  digitalWrite(OLED_RST, LOW); // set GPIO16 low to reset OLED
+  delay(50);
+  digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high
+
+  //initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  // display.setRotation(1);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA SENDER ");
+  display.display();
+
+
+  // ====
+  Serial.begin(115200);
+  // waits for serial to setup?
+  while (!Serial);
+
+  Serial.println();
+  Serial.println("LoRa Sender Test");
+
+  SPI.begin(SCK, MISO, MOSI, SS);
+  LoRa.setPins(SS, RST, DI0);
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  // LoRa.onReceive(cbk);
+  //  LoRa.receive();
+  // Serial.println("init ok");
+  // display.init();
+  // display.flipScreenVertically();
+  // display.setFont(ArialMT_Plain_10);
+
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+void loop() {
+   
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  //Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+  
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("LORA SENDER");
+  display.setCursor(0,20);
+  display.setTextSize(1);
+  display.print("LoRa packet sent.");
+  display.setCursor(0,30);
+  display.print("Counter:");
+  display.setCursor(50,30);
+  display.print(counter);      
+  display.display();
+
+  counter++;
+  
+  delay(10000);
+}
+
